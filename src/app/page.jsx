75b2d35c3f94c4131d304c3b186a6555a482844b
mdsalahuddin2001 <mdsalahuddin46464@gmail.com>
2024-03-07 06:47:18 +0600

    initial setup done

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl">Assalamu Alaikum</h1>
+      <Button>Good To Know</Button>
+    </div>
+  );
+}
